6600946777b22221a6a4ea8909a0d0f3259b70a4
Amalcxc <amalcxirsi@gmail.com>
2021-10-07 20:31:49 +0300

    Fix Linters

--- src/index.js
+++ src/index.js
@@ -1,54 +1,48 @@
 import './styles/styles.css';
-import 'regenerator-runtime/runtime'
+import 'regenerator-runtime/runtime';
 
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/QWe6fd15ksAlBbepEHeq/scores/';
 
+const refresh = document.querySelector('#refresh');
+const ul = document.querySelector('.ul');
 
-const base_url = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/QWe6fd15ksAlBbepEHeq/scores/"
-
-const refresh = document.querySelector("#refresh")
-const ul = document.querySelector(".ul")
-
-
-
-let array_of_scores = []
+let arrayOfScores = [];
 
 async function getscores() {
-    ul.innerHTML = '';
-    let response = await fetch(base_url);
-    let data = await response.json();
-    array_of_scores = data.result;
-    console.log(array_of_scores);
+  ul.innerHTML = '';
+  const response = await fetch(baseUrl);
+  const data = await response.json();
+  arrayOfScores = data.result;
 
-    array_of_scores.forEach((score) => {
-      const li = document.createElement('li');
-      li.innerHTML = `${score.user}: ${score.score}`;
-      li.className = 'list_item';
-      ul.append(li);
-    });
+  arrayOfScores.forEach((score) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${score.user}: ${score.score}`;
+    li.className = 'list-item';
+    ul.append(li);
+  });
 }
 
-refresh.addEventListener('click', getscores)
+refresh.addEventListener('click', getscores);
 
-
-const username = document.querySelector("#username")
-const score = document.querySelector("#score")
-const btn = document.querySelector("#submit")
-
+const username = document.querySelector('#username');
+const score = document.querySelector('#score');
+const btn = document.querySelector('#submit');
 
 async function addscores(e) {
   e.preventDefault();
- 
-  const response = await fetch(base_url, {
-    method: 'POST', 
+
+  const response = await fetch(baseUrl, {
+    method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       user: `${username.value}`,
       score: score.value,
     }),
   });
-  getscores()
+  getscores();
+  return response.json();
 }
 
 btn.addEventListener('click', addscores);
